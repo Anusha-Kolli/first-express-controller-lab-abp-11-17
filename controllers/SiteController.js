bbb1bd11fff31e1be5a6d94c37eda68f27e05980
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2018-02-07 19:03:42 +0000

    automatically backed up by learn

--- controllers/SiteController.js
+++ controllers/SiteController.js
@@ -3,3 +3,9 @@ let SiteController ={}
 SiteController.index = function(req,resp){
   resp.render('site/index');
 }
+SiteController.about = function(req,resp){
+  resp.render('site/about');
+}
+SiteController.contact = function(req,resp){
+  resp.render('site/contact');
+}
