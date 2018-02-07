502a0ae064c6116ef2c389eb86637964d0955c1b
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2018-02-07 19:06:28 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var express = require('express')
 var controller = require('./controllers/SiteController.js')
 var app = express()
 
-app.get('/',controller.index)
-app.get('/',controller.about)
-app.get('/',controller.contact)
+app.get('/',controller.Index)
+app.get('/',controller.About)
+app.get('/',controller.Contact)
 
 
 
